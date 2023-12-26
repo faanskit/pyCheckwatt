7d4afbf28b30cce4a491ec260a6d4afc85e7dd55
faanskit <marcus.karlsson@usa.net>
2023-12-26 12:53:47 +0000

    Fixed blocking bug, resolved references and allowed Pylance do its thing

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# setup.py
+"""Setup script for pyCheckwatt module."""
 from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.10"
@@ -15,8 +15,7 @@ setup(
     license="MIT",
     url="https://github.com/faanskit/pycheckwatt",
     download_url=f"https://github.com/faanskit/pycheckwatt/archive/v{VERSION}.tar.gz",
-    install_requires=["aiohttp", "pydantic"],
+    install_requires=["aiohttp", "pydantic", "relativedelta"],
     keywords=["checkwatt", "energyinbalance", "homeassistant"],
     python_requires=f">={MIN_PY_VERSION}",
 )
-
