221b315c72cfe2fdf9490127526cf2eecf323c86
faanskit <marcus.karlsson@usa.net>
2023-12-27 07:16:58 +0100

    Change e-mail

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.10"
 PACKAGES = find_packages()
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 setup(
     name="pycheckwatt",
@@ -11,7 +11,7 @@ setup(
     version=VERSION,
     description="A python library for communicating with CheckWatt EnergyInBalance",
     author="Marcus Karlsson",
-    author_email="marcus.karlsson@usa.net",
+    author_email="macke.karlsson@gmail.com",
     license="MIT",
     url="https://github.com/faanskit/pycheckwatt",
     download_url=f"https://github.com/faanskit/pycheckwatt/archive/v{VERSION}.tar.gz",
